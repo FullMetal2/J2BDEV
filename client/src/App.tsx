f76826b4c66971f5da325fbdc4ebfd8cbd6b011b
Jb <jean.baptiste.bertron@gmail.com>
2025-09-11 12:32:06 +0200

    responsive design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,37 @@
 export default function Maintenance() {
   return (
     <div className="container">
-      <span className="maintenance"> Maintenance en cours</span>
+      {/* Titre principal */}
+      <span className="maintenance">Maintenance en cours</span>
+
       <main>
+        {/* Logo */}
         <img src="/logo_j2bdev.webp" alt="Logo J2BDev" className="logo" />
 
-        <h1 className="title">J2BDev revient bientôt ✨</h1>
+        {/* Sous-titre */}
+        <h1 className="subtitle">J2BDev revient bientôt ✨</h1>
+
+        {/* Phrase */}
         <p className="phrase">
-          Je prépare une nouvelle version de mon portfolio (React + Node).\n
+          Je prépare une nouvelle version de mon portfolio (React + Node).
+          <br />
           Merci pour votre patience.
         </p>
 
+        {/* Liens */}
         <div className="contact">
           <p>
             Besoin de me joindre ?
-            <a href="mailto:contact@j2bdev.fr"> contact@j2bdev.fr </a> <br />
-            Suivez l'avancement sur :
+            <a href="mailto:contact@j2bdev.fr"> contact@j2bdev.fr </a>
+            <br />
+            Suivez l&apos;avancement sur :
             <a
               href="https://github.com/FullMetal2/J2BDEV"
               target="_blank"
               rel="noreferrer"
             >
               {" "}
-              GitHub{" "}
+              GitHub
             </a>
           </p>
         </div>
