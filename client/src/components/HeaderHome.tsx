a6aa130f5429e251e0a1793a253e6e8270d91a53
Jb <jean.baptiste.bertron@gmail.com>
2025-10-14 18:46:45 +0200

    rajout d'une image pour test mobile

--- client/src/components/HeaderHome.tsx
+++ client/src/components/HeaderHome.tsx
@@ -1,6 +1,7 @@
 import logo from "../assets/webp/images/logo_j2bdev.webp";
 import { NavLink } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import booki from "../assets/webp/thumbs/booki.webp";
 
 export default function HeaderHome() {
   return (
@@ -9,6 +10,7 @@ export default function HeaderHome() {
         className=" py-10 md:py-14 border-b border-white/10 md:mb-20 flex flex-col items-center justify-between md:flex-row"
         id="accueil"
       >
+        <img src={booki} alt="Booki" />
         <img src={logo} alt="Logo J2BDev" className="w-40 md:w-56 mb-3" />
         <h1 className=" text-neutral-400 sm:text-2xl lg:text-2xl text-center font-display font-semibold tracking-tight">
           Développeur web full-stack — React, Node, TypeScript
