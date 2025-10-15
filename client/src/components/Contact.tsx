65c60c51cecba71c5ce160440ab8b1f20231c4f0
Jb <jean.baptiste.bertron@gmail.com>
2025-10-15 11:28:15 +0200

    mise en place de emailJS

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { api, type ApiError } from "../lib/api";
+import { useRef } from "react";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 const appear = (d = 0) => ({
   initial: { opacity: 0, y: 12 },
@@ -10,33 +10,24 @@ const appear = (d = 0) => ({
 });
 
 export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-  const [status, setStatus] = useState<string | null>(null);
-  const [fieldErrors, setFieldErrors] = useState<Record<string, string[]>>({});
+  const form = useRef();
 
-  async function onSubmit(e: React.FormEvent) {
+  const sendEmail = (e) => {
     e.preventDefault();
-    setStatus(null);
-    setFieldErrors({});
-    try {
-      await api.sendContact({ name, email, message });
-      setStatus("✅ Message envoyé avec succès !");
-      setName("");
-      setEmail("");
-      setMessage("");
-    } catch (e: unknown) {
-      const err = e as ApiError;
-      const data = err.data as any;
-      if (data?.error?.fieldErrors) {
-        setFieldErrors(data.error.fieldErrors);
-        setStatus("❌ Erreurs de validation.");
-      } else {
-        setStatus("❌ Erreur réseau.");
-      }
-    }
-  }
+
+    emailjs
+      .sendForm("ID_SERVICE", "TEMPLATE_ID", form.current, {
+        publicKey: "KEY_PUBLIC",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
 
   return (
     <>
@@ -46,65 +37,53 @@ export default function Contact() {
             Me contacter
           </h2>
 
-          <form onSubmit={onSubmit} className="flex flex-col gap-10">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="flex flex-col gap-10"
+          >
             <div className="relative input-box w-full">
+              <label>Nom</label>
               <motion.input
                 type="text"
+                name="from_name"
                 required
                 placeholder="Nom"
                 {...appear(0)}
                 className="input w-full bg-slate-800 border border-slate-700 rounded-lg px-3 pt-2 py-2 text-white
                      focus:outline-none focus:border-blue-500 transition"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
               />
             </div>
-            {fieldErrors.name && (
-              <p className="text-red-400 text-sm">
-                {fieldErrors.name.join(", ")}
-              </p>
-            )}
+
             <div className="relative input-box w-full">
               <motion.input
                 type="email"
+                name="from_email"
                 placeholder="Email"
                 {...appear(0.1)}
                 className="input w-full bg-slate-800 border border-slate-700 rounded-lg px-3 pt-2 py-2 text-white
                    focus:outline-none focus:border-blue-500 transition"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            {fieldErrors.email && (
-              <p className="text-red-400 text-sm">
-                {fieldErrors.email.join(", ")}
-              </p>
-            )}
+
             <div className="relative input-box w-full">
               <motion.textarea
+                name="message"
                 placeholder="Votre message ..."
                 {...appear(0.2)}
                 className="input w-full bg-slate-800 border border-slate-700 rounded-lg px-3 py-2 min-h-32 text-white
                    focus:outline-none focus:border-blue-500 transition"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
               />
             </div>
-            {fieldErrors.message && (
-              <p className="text-red-400 text-sm">
-                {fieldErrors.message.join(", ")}
-              </p>
-            )}
 
             <motion.button
               type="submit"
+              value="Send"
               {...appear(0.3)}
               className="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-400 transition"
             >
               Envoyer
             </motion.button>
-
-            {status && <p className="mt-2">{status}</p>}
           </form>
         </div>
       </section>
