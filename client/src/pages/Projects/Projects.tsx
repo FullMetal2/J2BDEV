f4420775d7b174807a4a2b4b176483317ed8f4d6
Jb <jean.baptiste.bertron@gmail.com>
2026-03-09 14:37:34 +0100

    mise a jours SEO

--- client/src/pages/Projects/Projects.tsx
+++ client/src/pages/Projects/Projects.tsx
@@ -4,10 +4,15 @@ import Hero from "./components/Hero";
 import CTA from "./components/CTAProcess";
 import CompletedProject from "../../components/sections/Projects/CompletedProject";
 import TrainingProject from "../../components/sections/Projects/TrainingProject";
+import { Helmet } from "react-helmet-async";
 
 export default function ProjectsPage() {
   return (
     <>
+    <Helmet>
+      <title>Sites web pour artisans et indépendants | J2BDev Perpignan</title>
+      <meta name="description" content="Découvrez mes réalisations web faites sur mesure pour les artisans et indépendants — Perpignan et alentours"/>
+    </Helmet>
       <Header />
       <main className="min-h-screen flex flex-col py-8 md:py-8 mx-auto max-w-6xl px-5">
         <Hero />
